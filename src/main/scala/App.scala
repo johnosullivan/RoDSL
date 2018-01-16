f7a6e4c6030ce05eabeb3df66806af63e7ac364e
John O'Sullivan <josullivan1@luc.edu>
2018-01-16 13:36:47 -0600

    Minor edit

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,10 +1,15 @@
+
 /**
   * Created by johnosullivan on 12/12/17.
   */
 object App {
 
   def main(args: Array[String]): Unit = {
-    
+      println("Starting Ro-DSL")
+
+
+
+      println("Ended Ro-DSL")
   }
 
 }
